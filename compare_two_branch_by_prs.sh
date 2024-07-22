c78fa252564d77bb8e522b1e226c8b11972db19c
Yongqiang YANG <dataroaring@gmail.com>
2024-07-22 09:45:24 +0800

    add a scrift to compare two branches in github by prs

---
+++ compare_two_branch_by_prs.sh
@@ -0,0 +1,15 @@
+LEFT_BRANCH_NAME=$1
+RIGHT_BRANCH_BANE=$2
+
+PRS_IN_LEFT=`git log --oneline origin/master | awk '{print $NF}' | grep "(" | cut -c 2- | rev | cut -c 2- | rev`
+
+echo $PRS_IN_LEFT
+
+for PR in ${PRS_IN_LEFT}; do
+    MATCH_MSG_LINES=`git --no-pager log --grep "$PR" | wc -l`
+    if [ ${MATCH_MSG_LINES=} -eq 0 ]; then
+        git log $LEFT_BRANCH_NAME --grep "$PR"
+    fi
+done
+
+
